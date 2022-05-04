b72dd80b28b5ecb9ee247a4535ce8db03aa6df41
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-05-05 00:54:56 +0600

    item section added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "../Hero-section/HeroSection";
+import ItemSection from "../Item-section/ItemSection";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
+      <ItemSection />
     </div>
   );
 };
