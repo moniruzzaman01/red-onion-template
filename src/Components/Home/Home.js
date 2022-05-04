7f3d33f6094fb75e205d227840b347cf95ca0971
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-05-05 02:13:13 +0600

    speciality section added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "../Hero-section/HeroSection";
 import ItemSection from "../Item-section/ItemSection";
+import OurSpeciality from "../Our-speciality/OurSpeciality";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <ItemSection />
+      <OurSpeciality />
     </div>
   );
 };
---
+++ src/Components/Our-speciality/OurSpeciality.js
@@ -0,0 +1,102 @@
+import React from "react";
+import "./OurSpeciality.css";
+import image1 from "../../images/New folder/adult-blur-blurred-background-687824.png";
+import image2 from "../../images/New folder/chef-cook-food-33614.png";
+import image3 from "../../images/New folder/architecture-building-city-2047397.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowAltCircleRight } from "@fortawesome/free-solid-svg-icons";
+
+const OurSpeciality = () => {
+  return (
+    <div className="speciality-container container">
+      <h2>Why you choose us</h2>
+      <p>
+        Barton waited twenty always repair in within we do.An delighted
+        offending cutiodity my is dashwoods at. Boy prosperous increasing
+        surrounded.
+      </p>
+      <div className="speciality-items">
+        <div className="speciality-item">
+          <div className="speciality-img-container">
+            <img src={image1} alt="" />
+          </div>
+          <div className="contents-container">
+            <div className="icon">
+              <FontAwesomeIcon icon={faArrowAltCircleRight} />
+            </div>
+            <div className="contents">
+              <p>Fast Delivery</p>
+              <span>
+                Keep your system in sync eith automated web hook based
+                notifications each timelink is paid and how we dream about our
+                future.
+              </span>
+              <span className="seemoreBtn">
+                See more{" "}
+                <span>
+                  <FontAwesomeIcon
+                    icon={faArrowAltCircleRight}
+                  ></FontAwesomeIcon>
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="speciality-item">
+          <div className="speciality-img-container">
+            <img src={image2} alt="" />
+          </div>
+          <div className="contents-container">
+            <div className="icon">
+              <FontAwesomeIcon icon={faArrowAltCircleRight} />
+            </div>
+            <div className="contents">
+              <p>A Good Auto Responder</p>
+              <span>
+                Keep your system in sync eith automated web hook based
+                notifications each timelink is paid and how we dream about our
+                future.
+              </span>
+              <span className="seemoreBtn">
+                See more{" "}
+                <span>
+                  <FontAwesomeIcon
+                    icon={faArrowAltCircleRight}
+                  ></FontAwesomeIcon>
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="speciality-item">
+          <div className="speciality-img-container">
+            <img src={image3} alt="" />
+          </div>
+          <div className="contents-container">
+            <div className="icon">
+              <FontAwesomeIcon icon={faArrowAltCircleRight} />
+            </div>
+            <div className="contents">
+              <p>Home Delivery</p>
+              <span>
+                Keep your system in sync eith automated web hook based
+                notifications each timelink is paid and how we dream about our
+                future.
+              </span>
+              <span className="seemoreBtn">
+                See more{" "}
+                <span>
+                  <FontAwesomeIcon
+                    icon={faArrowAltCircleRight}
+                  ></FontAwesomeIcon>
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OurSpeciality;
